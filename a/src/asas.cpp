43a7614a988e8a2d54c8933bb5f3ee32cf4df167
Michael <mic3ael@gmail.com>
2014-06-29 18:22:10 +0300

    This c++ project

---
+++ a/src/asas.cpp
@@ -0,0 +1,18 @@
+/*
+ * asas.cpp
+ *
+ *  Created on: Jun 28, 2014
+ *      Author: mic3ael
+ */
+
+#include "asas.h"
+
+asas::asas() {
+	// TODO Auto-generated constructor stub
+
+}
+
+asas::~asas() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ a/src/asas.h
@@ -0,0 +1,17 @@
+/*
+ * asas.h
+ *
+ *  Created on: Jun 28, 2014
+ *      Author: mic3ael
+ */
+
+#ifndef ASAS_H_
+#define ASAS_H_
+
+class asas {
+public:
+	asas();
+	virtual ~asas();
+};
+
+#endif /* ASAS_H_ */
